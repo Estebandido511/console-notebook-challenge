37527531effecb48bf91d55192fb0e380d2cb88d
estebanechavarria <juanestebanechavarria511@gmail.com>
2025-02-17 13:13:33 -0500

    correcion de clase nota

--- app/console.py
+++ app/console.py
@@ -1,13 +1,10 @@
 # TODO: Agrega el código de las clases de la interfaz de usuario aquí. Borra este comentario al terminar.
 
-    class Note:
-
-        def __init__(self, high: str, medium: str, low: str):
-            self.high: str = high
-            self.medium: str = medium
-            self.low: str = low
 
 
-    class Notebook:
+
+
+
+
 
 
--- app/notebook.py
+++ app/notebook.py
@@ -1,1 +1,28 @@
 # TODO: Agrega el código de las clases del modelo aquí. Borra este comentario al terminar.
+
+class Note:
+
+    def __init__(self, HIGH: str, MEDIUM: str, LOW: str):
+        self.HIGH = "HIGH"
+        self.MEDIUM = "MEDIUM"
+        self.LOW = "LOW"
+
+    def __init__(self, code: int, title: str, text: str, importance: str):
+
+        self.code: int = code  #codigo unico de la nota
+        self.title: str = title  #titulo de la nota
+        self.text: str = text  #contenido de la nota
+        self.importance: str = importance  #nivel de importancia
+        self.creation_date: datetime.datetime = datetime.datetime.now()  # fecha de creación
+        self.tags: list[str] = []
+
+    def add_tag(self, tag: str) -> None:
+        """Agrega una etiqueta a la nota si no está repetida."""
+        if tag not in self.tags:
+            self.tags.append(tag)
+
+    def __str__(self) -> str:
+        """Devuelve un texto representando la nota."""
+        return f"Fecha: {self.creation_date}\n{self.title}: {self.text}"
+
+
